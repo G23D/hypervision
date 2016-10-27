f453d6a6ec4767f73c8e18eed793a4de2d32e2c3
eriestra <eriestra@wavelength.space>
2016-10-27 12:28:46 -0500

    3d Widgets for size and rotation in Y
    3d Widgets for size and rotation in Y

---
+++ Assets/EditSizeRotation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditSizeRotation : MonoBehaviour {
+
+    public void processMessage(string message) {
+
+        Vector3 localScale = transform.localScale;
+        Vector3 rotation = transform.rotation.eulerAngles;
+
+        if (message == "Size *1.025") {
+            localScale *= 1.025f;
+            transform.localScale = localScale;
+        }
+        if (message == "Size /1.025")
+        {
+            localScale /= 1.025f;
+            transform.localScale = localScale;
+        }
+        if (message == "RotY 5")
+        {
+            rotation.y += 5f;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        if (message == "RotY -5")
+        {
+            rotation.y -= 5f;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
---
+++ Assets/WidgetGazeSelect.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// FocusedObjectMessageReceiver class shows how to handle messages sent by FocusedObjectMessageSender.
+/// This particular implementatoin controls object appearance by changing its color when focused.
+/// </summary>
+public class WidgetGazeSelect : MonoBehaviour
+{
+
+    public GameObject RxGameObject;
+    public Color FocusedColor = Color.green;
+
+    private Material material;
+    private Color originalColor;
+
+    string lastCommand;
+    bool repeatLastCommand = false;
+    float repeatTime = 0.25f; // Repeat time in seconds
+    float ellapsedTime = 0.25f;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        originalColor = material.color;
+    }
+
+    private void Update() {
+
+        if (repeatLastCommand) {
+            if (ellapsedTime <= 0.0f){
+                RxGameObject.GetComponent<EditSizeRotation>().processMessage(lastCommand);
+                ellapsedTime = repeatTime;
+            }
+            else {
+                ellapsedTime -= Time.deltaTime;
+            }
+        } 
+    }
+
+    public void OnGazeEnter()
+    {
+        material.color = FocusedColor;
+        RxGameObject.GetComponent<EditSizeRotation>().processMessage(this.name);
+        lastCommand = this.name;
+        repeatLastCommand = true;
+    }
+
+    public void OnGazeLeave()
+    {
+        material.color = originalColor;
+        repeatLastCommand = false;
+        ellapsedTime = repeatTime;
+    }
+}
