6541579d9d4d7a89b128b47fbb1540f14312aacd
eriestrawl <eriestra@wavelength.space>
2016-11-01 15:54:13 -0600

    GUI - HL Networking
    Autodiscovery and connection of HL client to GUI Tablet server.

---
+++ Assets/Wavelength/RuntimeEditor/CustomNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public override void OnClientConnect(NetworkConnection conn) {
+	
+		Debug.Log ("OnClientConnect");
+	}
+
+	public override void OnServerConnect(NetworkConnection conn) {
+
+		Debug.Log ("OnServerConnect");
+	}
+}
