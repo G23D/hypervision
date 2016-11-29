3e927ce6e11d46d785dfb4041186b90af4bc4ab7
eriestrawl <eriestra@wavelength.space>
2016-11-29 11:33:02 -0600

    Including anchor managers
    Including anchor managers

--- Assets/Wavelength/Hypervision/RuntimeEditor/CustomNetworkManager.cs
+++ Assets/Wavelength/Hypervision/RuntimeEditor/CustomNetworkManager.cs
@@ -16,18 +16,18 @@ public class CustomNetworkManager : NetworkManager {
     const short InstructionMessageID = 888;
 
 	public override void OnClientConnect(NetworkConnection conn) {
-		Debug.Log ("OnClientConnect");
+		//Debug.Log ("OnClientConnect");
         client.RegisterHandler(InstructionMessageID, OnInstruction);
     }
 
 	public override void OnServerConnect(NetworkConnection conn) {
-		Debug.Log ("OnServerConnect");
+		//Debug.Log ("OnServerConnect");
 	}
 
     void OnInstruction(NetworkMessage netMsg)
     {
         InstructionMessage msg = netMsg.ReadMessage<InstructionMessage>();
-        Debug.Log("InstructionMessage " + msg.instruction);
+        //Debug.Log("InstructionMessage " + msg.instruction);
 
         // This code needs to be changed for a proper stack processor and eval function
 
--- Assets/Wavelength/Hypervision/RuntimeEditor/TapToPush.cs
+++ Assets/Wavelength/Hypervision/RuntimeEditor/TapToPush.cs
@@ -12,11 +12,52 @@ class SelectMessage : MessageBase {
 
 public class TapToPush : MonoBehaviour
 {
+    [Tooltip("Supply a friendly name for the anchor as the key name for the WorldAnchorStore.")]
+    public string SavedAnchorFriendlyName = "SavedAnchorFriendlyName";
+
+    /// <summary>
+    /// Manages persisted anchors.
+    /// </summary>
+    private WorldAnchorManager anchorManager;
+
+    /// <summary>
+    /// Controls spatial mapping.  In this script we access spatialMappingManager
+    /// to control rendering and to access the physics layer mask.
+    /// </summary>
+    private SpatialMappingManager spatialMappingManager;
+
     public bool taptest = false;
 
     public const short SelectMessageID = 889;
 
     public GameObject NetManager;
+
+
+    private void Start()
+    {
+        // Make sure we have all the components in the scene we need.
+        anchorManager = WorldAnchorManager.Instance;
+        if (anchorManager == null)
+        {
+            Debug.LogError("This script expects that you have a WorldAnchorManager component in your scene.");
+        }
+
+        spatialMappingManager = SpatialMappingManager.Instance;
+        if (spatialMappingManager == null)
+        {
+            Debug.LogError("This script expects that you have a SpatialMappingManager component in your scene.");
+        }
+
+        if (anchorManager != null && spatialMappingManager != null)
+        {
+            anchorManager.AttachAnchor(this.gameObject, SavedAnchorFriendlyName);
+        }
+        else
+        {
+            // If we don't have what we need to proceed, we may as well remove ourselves.
+            Destroy(this);
+        }
+    }
 
     public void OnSelect()
     {
